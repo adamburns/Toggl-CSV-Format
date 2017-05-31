aa71aa441362bdae9b9407688c6bef06512f34d1
Adam Burns <burns679@umn.edu>
2017-05-31 13:00:36 -0500

    Second commit

--- Toggl_Projects.py
+++ Toggl_Projects.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[22]:
+# In[14]:
 
 
 import matplotlib.pyplot as plt
@@ -10,28 +10,47 @@ import pandas as pd
 import re
 import time
 import easygui as e
+import os
+import sys
 get_ipython().magic(u'matplotlib inline')
 
-#Data import via pandas
-try:
-    toggle_preData = pd.read_csv("Toggl_projects_2016-01-01_to_2016-12-31.csv")
-except IOError:
-    e.msgbox("Could not read file 2016 Toggl CSV file. Please re-download that file and put it in the same directory as this Python script.", "Error")
-    exit()
-    
-try:
-    toggle_data = pd.read_csv("Toggl_projects_2017-01-01_to_2017-12-31.csv")
-except IOError:
-    e.msgbox("Could not read file 2016 Toggl CSV file. Please re-download that file and put it in the same directory as this Python script.", "Error")
-    exit()
+##
+#Attempts to data import via pandas, if the file does not exist, then error prompt
+#This operation occurs for both 2016 and 2017 data sets
+##
 
-toggle_data.columns=['Project type', 'Project', 'Hours', 'Resources', 'Private/public']
-toggle_preData.columns=['Project type', 'Project', 'Hours', 'Resources', 'Private/public']
-#Define the finite amount of Project Types
-projectStrings = ["ODD", "O&G", "Oil & Gas", "Reg A", "Essential", "Combined", "Interval Fund", 
-                  "BDC", "DST", "Conservation Easement","Fund", "REIT", "Energy"]
-statusStrings = ["Private", "Public", "NA"]
-resourceStrings = ["Internal", "External", "NA"]
+def dataLoad():
+    try:
+        global toggle_preData
+        toggle_preData = pd.read_csv("Toggl_projects_2016-01-01_to_2016-12-31.csv")
+        global toggle_data
+        toggle_data = pd.read_csv("Toggl_projects_2017-01-01_to_2017-12-31.csv")
+        toggle_data.columns=['Project type', 'Project', 'Hours', 'Resources', 'Private/public']
+        toggle_preData.columns=['Project type', 'Project', 'Hours', 'Resources', 'Private/public']
+        #Define the finite amount of Project Types
+        global projectStrings
+        projectStrings = ["ODD", "O&G", "Oil & Gas", "Reg A", "Essential", "Combined", "Interval Fund", 
+                          "BDC", "DST", "Conservation Easement","Fund", "REIT", "Energy"]
+        global statusStrings
+        statusStrings = ["Private", "Public", "NA"]
+        global resourceStrings
+        resourceStrings = ["Internal", "External", "NA"]
+    except IOError:
+        e.msgbox("Could not read file Toggl CSV file. Please re-download that file and put it in the same directory as this Python script.", "Error")
+        sys.exit()
+
+##
+#Iterates through the 2017 data, and populates data given from the functions above
+##
+def dataFormat():
+    for x in range(0, len(toggle_data['Project'])):
+        toggle_data['Project type'][x] = propertyType(toggle_data['Project'][x], 
+                                                      toggle_data['Project type'][x])
+        toggle_data['Hours'][x] = convertHours(toggle_data['Hours'][x]) + concatenateHours(toggle_data['Project'][x])
+        toggle_data['Private/public'][x] = statusType(toggle_data['Project'][x])
+        toggle_data['Resources'][x] = resourceType(toggle_data['Project'][x])
+
+        
 ##
 #Create function to iterate through projectStrings (Project Types), 
 #and determine if it is a substring of the project title.
@@ -63,8 +82,8 @@ def convertHours(str):
     return hour
 
 ##
-#
-#
+#If private in str, then it is private; same for public
+#otherwise "NA" is returned
 ##
 def statusType(str):
     for x in range(0, len(statusStrings)):
@@ -73,8 +92,8 @@ def statusType(str):
     return "NA"
 
 ##
-#
-#
+#If a project is ODD/Combined, then it does not have a resourceType
+#If a project contains (IC), then it is external, otherwise internal
 ##
 def resourceType(str):
     if "ODD".lower() in str.lower() or "Combined".lower() in str.lower():
@@ -85,33 +104,35 @@ def resourceType(str):
         return "Internal"
 
 ##
-#
-#
+#Given projects in 2017, concatenateHours(Str) looks them up in the 2016 projects
+#if there is a match, then the hours are combined
 ##
 def concatenateHours(str):
     for x in range(0, len(toggle_preData['Project'])):
         if str == toggle_preData['Project'][x]:
             return convertHours(toggle_preData['Hours'][x])
     return 0
-##
-#
-#
-##
-for x in range(0, len(toggle_data['Project'])):
-    toggle_data['Project type'][x] = propertyType(toggle_data['Project'][x], 
-                                                  toggle_data['Project type'][x])
-    toggle_data['Hours'][x] = convertHours(toggle_data['Hours'][x]) + concatenateHours(toggle_data['Project'][x])
-    toggle_data['Private/public'][x] = statusType(toggle_data['Project'][x])
-    toggle_data['Resources'][x] = resourceType(toggle_data['Project'][x])
+        
 
-toggle_data     
 ##
-#Data export function, retains only `Project` and `Registered time` columns 
-#and overwrites previous file
+#Tries data export function, retains only `Project` and `Registered time` columns 
+#and overwrites previous file. If not possible, then error prompt and system exit.
 ##
-toggle_base_url = "Toggl_projects_"
-toggle_timestamp = str(time.strftime("%m-%d-%y"))
-toggle_filetype = ".csv"
-toggle_url = toggle_base_url + toggle_timestamp + toggle_filetype
-toggle_data[['Project', 'Hours', 'Project type', 'Resources', 'Private/public']].to_csv(toggle_url,index=False)
+def dataExit():
+    try: 
+        toggle_base_url = "Toggl_projects_"
+        toggle_timestamp = str(time.strftime("%m-%d-%y"))
+        toggle_filetype = ".csv"
+        toggle_url = toggle_base_url + toggle_timestamp + toggle_filetype
+        toggle_data[['Project', 'Hours', 'Project type', 'Resources', 'Private/public']].to_csv(toggle_url,index=False)
+        os.remove("Toggl_projects_2017-01-01_to_2017-12-31.csv")
+        sys.exit()
+    except IOError:
+        e.msgbox("Could not read file 2016 Toggl CSV file. Please re-download that file and put it in the same directory as this Python script.", "Error")
+        sys.exit()
+
+if __name__ == '__main__':
+    dataLoad()
+    dataFormat()
+    dataExit()
 
